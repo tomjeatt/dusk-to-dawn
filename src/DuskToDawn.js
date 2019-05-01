3c0c95a1dc534d832e752b0a94b52ae87c5a19ec
Thomas Jeatt <tjeatt@zonedigital.com>
2019-05-01 19:42:28 +0100

    Added control/functions to get geo from user's current location

--- src/DuskToDawn.js
+++ src/DuskToDawn.js
@@ -3,35 +3,87 @@ import PropTypes from "prop-types";
 import GoogleMapsLoader from "./utilities/google-maps-loader";
 
 class DuskToDawn extends Component {
+  // If no value is passed property and value are set from property
+  updateState = (property, value) => {
+    this.setState(prevState => ({
+      ...prevState,
+      [property]: value
+    }));
+  };
+
+  handleAutocompleteResponse = () => {
+    const { autocomplete } = this.state;
+
+    autocomplete.addListener("place_changed", () => {
+      const place = autocomplete.getPlace();
+
+      // Retrieve geo data from place response
+      const {
+        geometry: {
+          location: { lat, lng }
+        }
+      } = place;
+
+      // Run geo functions to create latLng object
+      const userGeo = { lat: lat(), lng: lng() };
+
+      this.updateState("userGeo", userGeo);
+    });
+  };
+
   initialisePlacesApi = () => {
     GoogleMapsLoader.load(google => {
       const options = {
-        componentRestrictions: { country: ["gb"] },
         types: ["geocode"]
       };
 
+      // autocomplete field set as callback ref
       const autocomplete = new google.maps.places.Autocomplete(
         this.autocompleteField
       );
+
       autocomplete.setOptions(options);
+
+      // Only need to return geometry data as the latLng functions
+      // are what is needed from the response
       autocomplete.setFields(["geometry"]);
 
-      autocomplete.addListener("place_changed", () => {
-        const place = autocomplete.getPlace();
-        const {
-          geometry: {
-            location: { lat, lng }
-          }
-        } = place;
-
-        this.setState({
-          requestGeo: {
-            lat: lat(),
-            lng: lng()
-          }
-        });
-      });
+      this.setState(
+        {
+          autocomplete
+        },
+        () => {
+          this.handleAutocompleteResponse();
+        }
+      );
     });
+  };
+
+  getLocation = () => {
+    const options = {
+      enableHighAccuracy: true,
+      timeout: 5000,
+      maximumAge: 0
+    };
+
+    const success = position => {
+      const {
+        coords: { latitude, longitude }
+      } = position;
+
+      const userGeo = {
+        latitude,
+        longitude
+      };
+
+      this.updateState("userGeo", userGeo);
+    };
+
+    const error = err => {
+      console.warn(`ERROR(${err.code}): ${err.message}`);
+    };
+
+    navigator.geolocation.getCurrentPosition(success, error, options);
   };
 
   componentDidMount() {
@@ -49,13 +101,16 @@ class DuskToDawn extends Component {
           type="text"
           ref={ref => (this.autocompleteField = ref)}
         />
+        <button onClick={this.getLocation}>Get location</button>
       </div>
     );
   }
 }
 
 DuskToDawn.propTypes = {
-  requestGeo: PropTypes.object
+  autocompleteInstance: PropTypes.object,
+  geolocationAvailable: PropTypes.bool,
+  userGeo: PropTypes.object
 };
 
 export default DuskToDawn;
